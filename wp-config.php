ef2d1513284a0549f88f75beb7dd4ec427a7c94a
superKalo <superkalo91@gmail.com>
2015-02-21 17:43:33 +0200

    localhost remove plugins setting

--- wp-config.php
+++ wp-config.php
@@ -81,3 +81,5 @@ define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack t
 if ( !defined( 'ABSPATH' ) )
 	define( 'ABSPATH', dirname( __FILE__ ) . '/WordPress/' );
 require_once( ABSPATH . 'wp-settings.php' );
+
+define('FS_METHOD', 'direct');
