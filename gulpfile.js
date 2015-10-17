190c9cb8f57fa017f76c59f2cbba7f5360132c77
superKalo <superkalo91@gmail.com>
2015-10-18 01:20:38 +0300

    Added: elixir imagemin task to minify images Added: clarification comments in gulpfile.js Added: convert css stylesheet to scss to take advantage of elixir integrated Autoprefixer from scss to css Changed: ignore css/style.css, since we are using sass/style.scss from now on

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,48 @@
 var elixir = require('laravel-elixir');
 
+require('laravel-elixir-imagemin');
+
 config.assetsPath = './content/themes/easier-english-bg-theme';
 
 elixir(function(mix) {
     mix
+    /**
+     * Compile the main .scss file to .css
+     * During the compilation elixir AutoPrefixes the css
+     * http://laravel.com/docs/5.1/elixir#sass
+     */
+    .sass('style.scss', 'content/themes/easier-english-bg-theme/css/style.css')
+    /**
+     * Combine the css files into a single file
+     * http://laravel.com/docs/5.1/elixir#plain-css
+     */
     .styles([
-            '../../../lib/normalize-css/normalize.css',
-            'style.css'
-        ], 'content/themes/easier-english-bg-theme/css/style.min.css')
+        '../../../lib/normalize-css/normalize.css',
+        'style.css'
+    ], 'content/themes/easier-english-bg-theme/css/style.min.css')
+    /**
+     * Combine the js files into a single file
+     * http://laravel.com/docs/5.1/elixir#javascript
+     */
     .scripts([
-            'jquery.mmenu.min.js',
-            '../../../plugins/mailchimp/js/jquery.form.min.js',
-            'jquery.say.js',
+        'jquery.mmenu.min.js',
+        '../../../plugins/mailchimp/js/jquery.form.min.js',
+        'jquery.say.js',
 
-            'script.js'
-        ], 'content/themes/easier-english-bg-theme/js/script.min.js');
+        'script.js'
+    ], 'content/themes/easier-english-bg-theme/js/script.min.js')
+    /**
+     * Minify images with ImageMin: https://github.com/imagemin/imagemin
+     *  - gifsicle — Compress GIF images
+     *  - jpegtran — Compress JPEG images
+     *  - optipng — Compress PNG images
+     *  - svgo — Compress SVG images
+     * Use a Laravel Elixir wrapper for ImageMin
+     * https://github.com/nathanmac/laravel-elixir-imagemin
+     */
+    .imagemin(
+        './content/themes/easier-english-bg-theme/img-uncompressed',
+        './content/themes/easier-english-bg-theme/img',
+        { optimizationLevel: 3, progressive: true, interlaced: true }
+    );
 });
