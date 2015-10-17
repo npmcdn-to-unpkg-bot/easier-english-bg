034f4250b70b6b37926d72eb8310e0fb43329a45
superKalo <superkalo91@gmail.com>
2015-10-18 00:30:26 +0300

    Added: minify scripts by gulp/elixir, ignore scripts map file

--- gulpfile.js
+++ gulpfile.js
@@ -7,5 +7,12 @@ elixir(function(mix) {
     .styles([
             '../../../lib/normalize-css/normalize.css',
             'style.css'
-        ], 'content/themes/easier-english-bg-theme/css/style.min.css');
+        ], 'content/themes/easier-english-bg-theme/css/style.min.css')
+    .scripts([
+            'jquery.mmenu.min.js',
+            '../../../plugins/mailchimp/js/jquery.form.min.js',
+            'jquery.say.js',
+
+            'script.js'
+        ], 'content/themes/easier-english-bg-theme/js/script.min.js');
 });
